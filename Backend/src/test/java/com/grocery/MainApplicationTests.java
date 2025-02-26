e6eea504a75b9049eaddfb3de2552397c903c071
TylerHumb <101156725+TylerHumb@users.noreply.github.com>
2025-02-26 16:50:44 +1100

    setup basics of springboot backend

---
+++ Backend/src/test/java/com/grocery/MainApplicationTests.java
@@ -0,0 +1,14 @@
+package com.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("bruh");
+	}
+
+}
